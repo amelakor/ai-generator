332b71d228ae2221db2199d849306dc6246befd6
amelakor <amela_korjenic@hotmail.com>
2023-07-25 14:47:33 +0200

    API limit UI

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -62,3 +62,23 @@ export const checkApiLimit = async () => {
 
     return true;
 };
+
+export const getApiLimitCount = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return 0;
+    }
+
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userId,
+        },
+    });
+
+    if (!userApiLimit) {
+        return 0;
+    }
+
+    return userApiLimit.count;
+};
