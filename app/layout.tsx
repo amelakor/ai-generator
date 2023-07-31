3e118fce5763760b14d46a3a4b2f9237600f05d0
amelakor <amela_korjenic@hotmail.com>
2023-07-31 15:23:44 +0200

    Adding toast for errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/components/modal-provider";
+import { ToasterProvider } from "@/components/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
                     {children}
 
                     <ModalProvider />
+                    <ToasterProvider />
                 </body>
             </html>
         </ClerkProvider>
